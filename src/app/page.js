da2ba447c250310c4091263fc5937b873c0ee30b
Nathaniel <egbodonathaniel@yahoo.com>
2025-04-11 17:27:36 +0100

    build service section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,12 @@
 import Contact from "@/app/components/Contact";
+import Service from "@/app/components/Service";
 
 export default function Home() {
   return (
     // call components here <component_name />
     <div>
       <h1>Hello World</h1>
+      <Service />
       <Contact />
     </div>
   );
