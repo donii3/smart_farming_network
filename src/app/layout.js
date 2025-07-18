2fece990b729c3b05c649b9c64d3c38d058ff986
Nathaniel <egbodonathaniel@yahoo.com>
2025-07-18 17:34:55 +0100

    resolve dynamic page routing

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,26 @@
-
 import "@/app/globals.css";
-
 // export const metadata = {
 //   title: "Smart Farming Network",
 //   description: "A smart agricultural application",
 // };
+import ClientScripts from "@/components/ClientScripts";
+import Script from "next/script";
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body suppressHydrationWarning>
-        
-        {children}
-
+      <head>
+        <Script
+          src="/assets/js/jquery-3.6.0.min.js"
+          strategy="beforeInteractive"
+        />
+      </head>
+      <body>        
+        <div id="app-root">
+          {children}
+          <ClientScripts />
+        </div>
       </body>
     </html>
   );
