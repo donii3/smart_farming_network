1eac6c720a6cdea990f7d99a5165132aa651e0a8
Nathaniel <egbodonathaniel@yahoo.com>
2025-07-25 22:19:49 +0100

    add preloader for route transition

--- src/app/(auth)/layout.js
+++ src/app/(auth)/layout.js
@@ -4,12 +4,13 @@ import { Suspense } from "react";
 import Preloader from "@/components/Preloader";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import loading from "../loading";
 
 
 export default function AuthLayout({ children }) {
   return (
     <div id="auth-layout">
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<loading />}>
         <Preloader />
       </Suspense>
       <Header />
